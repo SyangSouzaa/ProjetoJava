4ec707880a79d714d076002a248ebd3f627db326
SyangSouzaa <syangsouza05@gmail.com>
2026-03-11 20:51:45 -0300

    Projeto da Apostila 3

---
+++ Apostila03/src/Master.java
@@ -0,0 +1,23 @@
+public class Master {
+
+    public static void main(String[] args){
+        System.out.println("Bom dia!");
+
+        //Declarar variaveis no Java
+
+        short idade;
+        //Atribuindo um valor para a variavel
+        idade = 32;
+        //Declarar a variavel e atribuir um valor
+
+        int anoNascimento = 1994;
+        double salario = 16000.50;
+        float altura = 16000.50f;
+        char incialNome = 'L';
+        boolean maiorIdade = true; //false
+        String nome = "Lucas";
+    }
+
+}
+
+

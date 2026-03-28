a9ab1e4186b9742d6ae3e7ba729d8273f19fb9cb
SyangSouzaa <syangsouza05@gmail.com>
2026-03-27 21:10:26 -0300

    Adicionando Apostila04-Exercicios

---
+++ Apostila04-Exercicios/src/br/com/fiap/exercicio01/view/Terminal.java
@@ -0,0 +1,46 @@
+package br.com.fiap.exercicio01.view;
+
+import br.com.fiap.exercicio01.model.Aluno;
+
+import java.util.Scanner;
+
+public class Terminal {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Ler os dados dos alunos
+        System.out.println("Digite o nome do aluno: ");
+        String nome = entrada.next() + entrada.nextLine();
+
+        System.out.println("Digite a nota da CP1: ");
+        double cp1 = entrada.nextDouble();
+
+        System.out.println("Digite a nota da CP2: ");
+        double cp2 = entrada.nextDouble();
+
+        System.out.println("Digite a nota da Sprint 1: ");
+        double sprint1 = entrada.nextDouble();
+
+        System.out.println("Digite a nota da Sprint 2: ");
+        double sprint2 = entrada.nextDouble();
+
+        System.out.println("Digite a nota da GS: ");
+        double gs = entrada.nextDouble();
+
+        //Criar o objeto aluno
+        Aluno aluno = new Aluno();
+        aluno.Nome = nome;
+        aluno.cp1 = cp1;
+        aluno.cp2 = cp2;
+        aluno.sprint1 = sprint1;
+        aluno.sprint2 = sprint2;
+        aluno.gs = gs;
+
+        //Atribuir os valores aos atributos do objeto aluno
+        double media = aluno.calcularMedia();
+
+        //Calcular e exibir média
+        System.out.println("Médiado aluno " + aluno.Nome + " é " + media);
+    }
+
+}

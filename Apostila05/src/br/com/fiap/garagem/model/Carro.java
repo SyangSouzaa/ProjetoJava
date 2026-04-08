89b4dea0881bc913bb6bc4675788c3e21b671599
SyangSouzaa <syangsouza05@gmail.com>
2026-04-08 19:50:29 -0300

    Adicionando objeto

--- Apostila05/src/br/com/fiap/garagem/model/Carro.java
+++ Apostila05/src/br/com/fiap/garagem/model/Carro.java
@@ -1,4 +1,17 @@
 package br.com.fiap.garagem.model;
 
 public class Carro {
+
+        public String placa;
+        int numeroVaga;
+        boolean preferencial;
+        char setor;
+        String horaEntrada;
+        String horaSaida;
+        double valor;
+
+        double calcularValorPagar(){
+            return 0;
+        }
+
 }
---
+++ Apostila05/src/br/com/fiap/garagem/view/Terminal.java
@@ -0,0 +1,12 @@
+package br.com.fiap.garagem.view;
+
+import br.com.fiap.garagem.model.Carro;
+
+public class Terminal {
+
+    public static void main(String[] args) {
+        //Criar um objeto carro
+        Carro onix = new Carro();
+        onix.placa = "ABC1234";
+    }
+}

6ec0e5880d78980e07252a3c38e488829d553e53
SyangSouzaa <syangsouza05@gmail.com>
2026-04-08 19:56:33 -0300

    Adicionando métodos getters e setters

--- Apostila05/src/br/com/fiap/garagem/model/Carro.java
+++ Apostila05/src/br/com/fiap/garagem/model/Carro.java
@@ -2,16 +2,25 @@ package br.com.fiap.garagem.model;
 
 public class Carro {
 
-        public String placa;
-        int numeroVaga;
-        boolean preferencial;
-        char setor;
-        String horaEntrada;
-        String horaSaida;
-        double valor;
+        private String placa;
+        private int numeroVaga;
+        private boolean preferencial;
+        private char setor;
+        private String horaEntrada;
+        private String horaSaida;
+        private double valor;
 
         double calcularValorPagar(){
             return 0;
         }
 
+        //Métodos Getters e Setter
+        public double getValor(){
+                return valor;
+        }
+
+        public void setValor(double valor){
+                valor = valor;
+        }
+        
 }

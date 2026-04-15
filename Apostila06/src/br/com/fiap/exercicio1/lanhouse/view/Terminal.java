3a54c4039e2473d6079a02b27f4121ef0c34f4a1
SyangSouzaa <syangsouza@gmail.com>
2026-04-15 20:57:19 -0300

    Adicionando Apostila06

---
+++ Apostila06/src/br/com/fiap/exercicio1/lanhouse/view/Terminal.java
@@ -0,0 +1,14 @@
+package br.com.fiap.exercicio1.lanhouse.view;
+
+import br.com.fiap.exercicio1.lanhouse.model.Computador;
+
+public class Terminal {
+    public static void main(String[] args) {
+
+
+        Computador pc = new Computador("Intel 7", 1, 1024, "RTX 5060", false, 32);
+
+        //Criar um novo objeto Computador
+        Computador pichau = new Computador("19", 2);
+    }
+}
---
+++ Apostila06/src/br/com/fiap/exercicio2/model/Profissao.java
@@ -0,0 +1,13 @@
+package br.com.fiap.exercicio2.model;
+
+public class Profissao {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
